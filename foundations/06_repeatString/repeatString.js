15c8e98dcab8afc31a27487d1700a9dfb5eaa058
candejt <139970803+candejt@users.noreply.github.com>
2026-03-09 20:51:47 +0100

    Complete 06 repeat string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(item,times) {
+    if (times <0) return "ERROR"
+    let string=""
+    for (let i=0; i<times; i++){
+        string += item
+    }
+    return string
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
