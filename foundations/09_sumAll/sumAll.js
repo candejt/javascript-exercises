fe37f71e676d7bdb83571744cfb8973800c9691d
candejt <139970803+candejt@users.noreply.github.com>
2026-03-10 15:27:13 +0100

    Complete 09 sum all

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (a<0 || b<0 || typeof a !=="number"|| typeof b !=="number" || !Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR"
+    }
+    let start = Math.min(a,b)
+    let end = Math.max(a,b)
 
+    let totalSum = 0
+
+    for (let i= start; i<=end; i++){
+        totalSum += i
+    }
+    return totalSum
 };
 
 // Do not edit below this line
