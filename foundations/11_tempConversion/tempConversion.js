e830b13f9042b32a54817d52ed8e53ff58f1b8ea
candejt <139970803+candejt@users.noreply.github.com>
2026-03-11 10:31:30 +0100

    Complete 11 temp conversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp-32) * (5/9)
+  let cel = Math.round (conversion*10)/10
+  return cel
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = (temp * (9/5) + 32)
+  let fahr = Math.round (conversion*10)/10
+  return fahr
 };
 
 // Do not edit below this line
