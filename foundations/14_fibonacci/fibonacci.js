a4b2d2d0c2437dcfdb8f01e544dd387a2eb29436
candejt <139970803+candejt@users.noreply.github.com>
2026-03-14 17:29:13 +0100

    Complete 14 finonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    const n = typeof count!== 'number' ? parseInt(count): count
+    if (n<0) return "OOPS"
+    if (n===0)return 0
 
+    let a=0
+    let b=1
+    for (let i=1; i<n; i++){
+        const temp=b
+        b=b+a
+        a=temp
+    }
+    return b
 };
 
 // Do not edit below this line
