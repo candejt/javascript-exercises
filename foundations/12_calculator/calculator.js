664a255273212c6652b929a868b5e94211e421d5
candejt <139970803+candejt@users.noreply.github.com>
2026-03-14 16:32:12 +0100

    Complete 12 calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	const total= a+b
+  return total
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  const total=a-b
+  return total	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const total=arr.reduce((acc, curr)=>{
+    return acc+curr
+  }, 0)
+  return total
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  const total = arr.reduce((acc,curr)=>{
+    return acc * curr
+  }, 1)
+  return total
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	const total = a**b
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1
+  for (let i=a; i>0; i--){
+    total=total*i
+  }
+  return total
 };
 
 // Do not edit below this line
