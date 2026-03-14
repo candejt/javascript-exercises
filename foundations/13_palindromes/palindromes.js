17bb43057f338075638199d5382dc106561177a9
candejt <139970803+candejt@users.noreply.github.com>
2026-03-14 16:53:10 +0100

    Complete 13 palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alpha='abcdefghijklmnopqrstuvwxyz0123456789'
+    const cleanString= string
+        .toLowerCase()
+        .split('')
+        .filter((char)=>alpha.includes(char))
+        .join('')
+    const stringReverse= cleanString.split('').reverse().join('')
+    return cleanString===stringReverse
 };
 
 // Do not edit below this line
