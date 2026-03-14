cd8b9535bf3cc2a837b1fc5bbdec253d206f8994
candejt <139970803+candejt@users.noreply.github.com>
2026-03-14 18:11:59 +0100

    Complete 16 find the oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const getAge=function(birth,death){
+    if (!death){
+        death=new Date().getFullYear()
+    }
+    return death - birth
+}
+const findTheOldest = function(people) {
+    return people.reduce ((acc, curr)=>{
+        const accAge= getAge(acc.yearOfBirth, acc.yearOfDeath)
+        const currAge=getAge(curr.yearOfBirth, curr.yearOfDeath)
+       
+        return accAge < currAge ? curr : acc 
+    })
 
 };
 
