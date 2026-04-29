7cbef4419c03a25af5e879cbd137f4987eb797ad
candejt <139970803+candejt@users.noreply.github.com>
2026-04-29 15:09:42 +0200

    Solve 2 contain

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,19 @@
-const contains = function() {
+function contains (object, value) {
+  for (let key in object){
+    if (object[key] === value || (Number.isNaN(object[key]) && Number.isNaN(value))){
+        return true;
+    }
+    if (typeof object[key] === 'object' && object[key] !== null){
+        if (contains (object[key],value)){
+            return true;
+        }
+    }
+  }
+  return false;
   
 };
   
 // Do not edit below this line
 module.exports = contains;
+
+
