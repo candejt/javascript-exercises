7487db37fe5026b1075f41ff72578a29b7786556
candejt <139970803+candejt@users.noreply.github.com>
2026-04-29 12:46:41 +0200

    Solve 1 factorial using recursion

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,14 @@
-const factorial = function() {
-
-};
+function factorial(n) {
+  if (n < 0 || n % 1 !== 0 || typeof n !== "number") {
+    return undefined;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
 
 // Do not edit below this line
 module.exports = factorial;
+
+console.log(factorial(5));
