cbf0bc51268ab29a07365c1415a1c7bf3397c44e
candejt <139970803+candejt@users.noreply.github.com>
2026-04-30 10:47:29 +0200

    Solve 3 total integers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,23 @@
-const totalIntegers = function() {
-  
+
+const isObject = (value) => typeof value === "object" && value!== null;
+
+function totalIntegers(data) {
+  if (!isObject(data)) {
+    return;
+  }
+
+  let count = 0;
+  for (let item of Object.values(data)){
+    if (Number.isInteger(item)){
+        count++;
+    }else if (isObject(item)){
+        count += totalIntegers(item)
+    }
+  }
+  return count
 };
   
 // Do not edit below this line
 module.exports = totalIntegers;
+
+totalIntegers([[5], 3])
