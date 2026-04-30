98e147278454852c8896c9808480b9b6d52a438e
candejt <139970803+candejt@users.noreply.github.com>
2026-04-30 11:54:12 +0200

    Solve 4 permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,25 @@
-const permutations = function() {
+function permutations(array) {
+
+    if (array.length === 0){
+        return [[]];
+    }
   
+    let result = [];
+    for (let i=0; i<array.length; i++){
+        const currentNum = array[i];
+
+        const remainingNums = array.slice(0, i).concat(array.slice(i + 1));
+
+        const remainingPermutations = permutations(remainingNums);
+
+        for (let p of remainingPermutations) {
+            result.push([currentNum, ...p]);
+        }
+    }
+    return result;
 };
   
 // Do not edit below this line
 module.exports = permutations;
+
+
