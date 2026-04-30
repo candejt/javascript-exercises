b24ed0831de99e02fc79dc29fb999bc6280406dd
candejt <139970803+candejt@users.noreply.github.com>
2026-04-30 13:29:39 +0200

    Solve 5 pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,19 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+  if (n===1){
+    return [1]
+  }
+  const prevRow = pascal(n-1);
+  let currentRow = [1];
+
+  for (let i=0; i< prevRow.length-1; i++){
+    currentRow.push(prevRow[i] + prevRow [i+1])
+  }
+  currentRow.push(1);
+  return currentRow;
 };
   
 // Do not edit below this line
 module.exports = pascal;
+
+
+pascal (4)
